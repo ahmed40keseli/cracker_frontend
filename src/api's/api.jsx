8bc2d82e2d689f074473847e16c6b7d78ea958cd
ahmed40keseli <krkkseli@gmail.com>
2025-01-15 09:49:34 +0300

    try server connection

--- src/api's/api.jsx
+++ src/api's/api.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 const API = axios.create({
   // baseURL: "todo.crmpanel.tr", // Backend URL
   // baseURL: "http://localhost:8081",
-  baseURL: "https://todo.crmpanel.tr",
+  baseURL: "http://todo.crmpanel.tr:8081/api",
 
   // api adresi belirlenir
 });
